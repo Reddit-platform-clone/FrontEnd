6ec8a999e7f473bf55663f9de59b51dc7ded7de8
atarek993 <ahmed.hagras02@eng-st.cu.edu.eg>
2024-03-02 22:11:45 +0200

    starting createpost page

---
+++ sarakel/src/Createpostpage/Createpost.js
@@ -0,0 +1,17 @@
+
+function Createpost(){
+    return(
+        <body className="body">
+             <div className="Title">
+               <h2 className="createpost">Create Post</h2>
+               <hr/>
+                <label for="Text">Text</label><br/>
+                 <input type='text' id="Text" placeholder="Text(optional)"></input>
+            
+             </div>
+
+        </body>
+       
+    );
+}
+export default Createpost
