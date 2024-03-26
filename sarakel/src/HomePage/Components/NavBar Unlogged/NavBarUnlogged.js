5223fec9ee54f06d9de837e1327f81334eec9b0d
KareemAbdelnabi <97126849+KareemAbdelnabi@users.noreply.github.com>
2024-03-26 07:26:28 +0200

    feat : rendered navigation bar correctly
    shared state between files to render the correct navigation bar

--- sarakel/src/HomePage/Components/NavBar Unlogged/NavBarUnlogged.js
+++ sarakel/src/HomePage/Components/NavBar Unlogged/NavBarUnlogged.js
@@ -1,60 +1,60 @@
+// Import necessary dependencies
 import React, { useState } from 'react';
 import './NavBarUnlogged.css';
-import logo from "./pngwing.com.png"
-import './bootstrap.min.css'
+import logo from "./pngwing.com.png";
+import './bootstrap.min.css';
 import { IoSearchOutline } from "react-icons/io5";
-import { IoMdCloseCircleOutline } from "react-icons/io";
-import LogIn from "../LogIn2/LogIn.js"
+import LogIn from "../LogIn/LogIn";
 
+// Your components
 
+function NavBarUnlogged({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(false);
 
-function NavBar() {
-  const[ShowLogin,setLogin]=useState(false);
+  const handleLogin = () => {
+    setShowLogin(true);
+  };
 
-  const handleLogin = () =>{
-    setLogin(true);
-  }
-
-  if(ShowLogin){
-    return <LogIn />
-  }
-
+  // After successful login, call onLogin function passed from parent component
+  const handleSuccessfulLogin = () => {
+    onLogin();
+  };
 
   return (
     <div className='container-fluid'>
-    <div className="navbar sticky-top">
-    <div className="left">
-          {/* <div className="hamburger-menu" data-title="Open navigation">&#9776;</div> */}
+      <div className="navbar sticky-top">
+        <div className="left">
           <img src={logo} className="img" alt="Logo" />
           <a href="#" className="logo" >Sarakel</a>
         </div>
 
-      <div className="center">
-  <label htmlFor="search" className="search-label" data-title="Search Sarakel"></label>
-  <div className="search-icon-container">
-    <IoSearchOutline id="search-icon" />
-  </div>
-  <input type="text" id="search" placeholder="search sarakel" className="search" />
-</div>
+        <div className="center">
+          <label htmlFor="search" className="search-label" data-title="Search Sarakel"></label>
+          <div className="search-icon-container">
+            <IoSearchOutline id="search-icon" />
+          </div>
+          <input type="text" id="search" placeholder="search sarakel" className="search" />
+        </div>
 
-      
-      <div className="right">
-      <button className='right-login-btn' onClick={handleLogin}>Login</button>
-      
-        <div className="dropdown">
-          <button className="dropbtn" >&#8226;&#8226;&#8226;</button>
-          <div className='dropdown-content'>
-            <a href="#">Option 1</a>
-            <a href="#">Option 2</a>
-            <a href="#">Option 3</a>
+        <div className="right">
+          <button className='right-login-btn' onClick={handleLogin}>Login</button>
+
+          <div className="dropdown">
+            <button className="dropbtn">&#8226;&#8226;&#8226;</button>
+            <div className='dropdown-content'>
+              <a href="#">Option 1</a>
+              <a href="#">Option 2</a>
+              <a href="#">Option 3</a>
+            </div>
           </div>
         </div>
       </div>
-      
-    </div>
-    <hr className="m-0" />
+      <hr className="m-0" />
+
+      {/* Render login modal if showLogin is true */}
+      {showLogin && <LogIn onSuccessfulLogin={handleSuccessfulLogin} />}
     </div>
   );
 }
 
-export default NavBar;
+export default NavBarUnlogged;
--- sarakel/src/HomePage/HomePage.js
+++ sarakel/src/HomePage/HomePage.js
@@ -1,12 +1,20 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import NavBar from './Components/NavBar/NavBar'
 import SideBar from './Components/SideBar/SideBar'
 import Content from './Components/content/content'
+import NavBarUnlogged from 'C:\\Users\\moham\\Desktop\\sarakel\\sarakel\\src\\HomePage\\Components\\NavBar Unlogged\\NavBarUnlogged';
 
 export default function HomePage(){
-    return(
-        <div>
-            <NavBar />
+    
+        const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+        const handleLogin = () => {
+          setIsLoggedIn(true);
+        };
+      
+        return (
+          <div>
+            {isLoggedIn ? <NavBar /> : <NavBarUnlogged onLogin={handleLogin} />}
             <SideBar />
             <Content />
         </div>
