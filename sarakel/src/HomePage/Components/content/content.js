29defe3389ad7ba90835dde932ba76a8b217ed56
KareemAbdelnabi <97126849+KareemAbdelnabi@users.noreply.github.com>
2024-04-16 21:31:58 +0200

    feat : backend integration

--- sarakel/src/HomePage/Components/content/content.js
+++ sarakel/src/HomePage/Components/content/content.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import jsonData from "../mock.json";
 import "./content.css";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { BsViewList, BsViewStacked } from "react-icons/bs";
@@ -10,30 +9,43 @@ import { CiViewList } from "react-icons/ci";
 import { AiOutlinePicture } from "react-icons/ai";
 import ImageSlider from "./imageSlider";
 import { RiVideoFill } from "react-icons/ri";
-import { useAuth } from '../AuthContext.js';//import
-
-
+import { useAuth } from "../AuthContext.js"; //import
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Content = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [communities, setCommunities] = useState([]);
   const [hiddenPosts, setHiddenPosts] = useState({});
   const [joinStates, setJoinStates] = useState({});
-  const[saveStates, setSaveStates]= useState({});
+  const [saveStates, setSaveStates] = useState({});
   const [viewType, setViewType] = useState("card");
   const [showSortOptions, setShowSortOptions] = useState(false);
+  const [sortingType, setSortingType] = useState("best");
   const [showViewOptions, setShowViewOptions] = useState(false);
-  const { token } = useAuth();//init
-
+  const { token } = useAuth(); //init
 
   useEffect(() => {
-    const fetchData = () => {
-      setTimeout(() => {
-        setPosts(jsonData.posts);
-        setCommunities(jsonData.communities);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/subreddit/get${
+            sortingType.charAt(0).toUpperCase() + sortingType.slice(1)
+          }`
+        );
+        const responseData = await response.json();
+        if (response.ok) {
+          setPosts(responseData.data);
+          setLoading(false);
+        } else {
+          throw new Error(
+            responseData.message || "Failed to fetch posts"
+          );
+        }
+      } catch (error) {
+        console.error("Error fetching posts:", error);
         setLoading(false);
-      }, 1000);
+      }
     };
 
     fetchData();
@@ -41,30 +53,68 @@ const Content = () => {
     return () => {
       // Cleanup tasks if needed
     };
-  }, []);
+  }, [sortingType]); // Refetch posts when sortingType changes
 
-  const getCommunityName = (communityId) => {
-    const community = communities.find((c) => c.id === communityId);
-    return community ? community.name : "Unknown";
+  const handleSortTypes = () => {
+    setShowSortOptions(!showSortOptions);
+    setShowViewOptions(false);
   };
 
-  const getCommunityImage = (communityId) => {
-    const community = communities.find((c) => c.id === communityId);
-    return community ? community.image : "default_community_image_url";
+  const handleSortingOption = (option) => {
+    setSortingType(option.toLowerCase());
+    setShowSortOptions(false);
   };
 
+
+  // const handleVoteClick = async (postId, rank) => {
+  //   if (!token) {
+  //     toast.error("You need to Login first");
+  //     return;
+  //   }
+  //   try {
+  //     const response = await fetch("http://localhost:5000/api/vote", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${token}`,
+  //       },
+  //       body: JSON.stringify({
+  //         rank: rank, // Rank can be 1 for upvote, 0 to clear vote, or -1 for downvote
+  //         type: "post",
+  //         entityId: "661d88eff83edb1b16c0c394",
+  //       }),
+  //     });
+  //     console.log("post id is :",postId)
+  //     console.log("response : ",response)
+  
+  //     // Check for the status code
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! Status: ${response.status}`);
+  //     }
+  
+  //     const responseData = await response.json();
+  //     console.log(responseData.message);
+  //   } catch (error) {
+  //     console.error("Error voting:", error);
+  //   }
+  // };
+  
+  
+  
   const handleUpvoteClick = (postId) => {
+    console.log("upvote")
+    console.log("post id : ",postId)
     setPosts((prevPosts) =>
       prevPosts.map((post) =>
-        post.id === postId
+        post.postId === postId
           ? {
-            ...post,
-            upvoted: !post.upvoted,
-            downvoted: false,
-            likes: post.upvoted
-              ? parseInt(post.likes) - 1
-              : parseInt(post.likes) + 1, // Parse likes to integer
-          }
+              ...post,
+              upvoted: !post.upvoted,
+              downvoted: false,
+              likes: post.upvoted
+                ? parseInt(post.likes) - 1
+                : parseInt(post.likes) + 1,
+            }
           : post
       )
     );
@@ -73,106 +123,189 @@ const Content = () => {
   const handleDownvoteClick = (postId) => {
     setPosts((prevPosts) =>
       prevPosts.map((post) =>
-        post.id === postId
+        post.postId === postId
           ? {
-            ...post,
-            upvoted: false,
-            downvoted: !post.downvoted,
-            likes: post.downvoted
-              ? parseInt(post.likes) + 1
-              : parseInt(post.likes) - 1,
-          }
+              ...post,
+              upvoted: false,
+              downvoted: !post.downvoted,
+              likes: post.downvoted
+                ? parseInt(post.likes) + 1
+                : parseInt(post.likes) - 1,
+            }
           : post
       )
     );
   };
 
-  const handleJoinClick = (postId) => {
-    setJoinStates((prevState) => ({
-      ...prevState,
-      [postId]: !prevState[postId],
-    }));
+  const getPostInfo = (postId) => {
+    const post = posts.find((post) => post.postId === postId);
+    return post ? { name: post.name, userId: post.userId } : null;
   };
 
-  const handleSaveClick = (postId) => {
-    setSaveStates((prevState) => ({
-      ...prevState,
-      [postId]: !prevState[postId],
-    }));
+  const handleJoinClick = async (postId) => {
+    if (!token) {
+      toast.error("You need to Login first");
+      return;
+    }
+    try {
+      const { name, userId } = getPostInfo(postId);
+      const isJoining = !joinStates[postId];
+      const response = await fetch(
+        `http://localhost:5000/api/community/${
+          isJoining ? "join" : "leave"
+        }`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({
+            communityName: name,
+          }),
+        }
+      );
+      const responseData = await response.json();
+      if (response.ok) {
+        console.log(responseData.message);
+        setJoinStates((prevState) => ({
+          ...prevState,
+          [postId]: isJoining,
+        }));
+      } else {
+        throw new Error(
+          responseData.message || "Failed to join/leave community"
+        );
+      }
+    } catch (error) {
+      console.error("Error joining/leaving community:", error);
+    }
   };
+
+  const handleSaveClick = async (postId) => {
+
+    if (!token) {
+      toast.error("You need to Login first");
+      return;
+    }
+    // Check if the post is already saved
+    const isSaved = saveStates[postId];
+  
+    try {
+      const response = await fetch(`http://localhost:5000/api/${isSaved ? 'unsave' : 'save'}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          type: "post",
+          entityId: postId,
+        }),
+      });
+      const responseData = await response.json();
+      if (response.ok) {
+        console.log(responseData.message);
+        // Toggle the save state for the post
+        setSaveStates((prevState) => ({
+          ...prevState,
+          [postId]: !isSaved,
+        }));
+      } else {
+        throw new Error(responseData.message || "Failed to save/unsave post");
+      }
+    } catch (error) {
+      console.error("Error saving/unsaving post:", error);
+    }
+  };
+  
+  const handleReportClick = async (postId, userId) => {
+    if (!token) {
+      toast.error("You need to Login first");
+      return;
+    }
+  
+    try {
+      const response = await fetch("http://localhost:5000/api/report", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          reason: "dummy",
+          type: "post",
+          entityId: postId,
+          reportedUsername: userId,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const responseData = await response.json();
+      console.log(responseData.message);
+    } catch (error) {
+      console.error("Error reporting post:", error);
+    }
+  };
+  
 
   const handleHideClick = (postId) => {
+    console.log(posts)
     setHiddenPosts((prevHiddenPosts) => ({
       ...prevHiddenPosts,
       [postId]: true,
     }));
   };
 
-  const handleSortTypes = () => {
-    setShowSortOptions(!showSortOptions);
-    setShowViewOptions(false);
-  };
-
   const handleViewTypes = () => {
     setShowViewOptions(!showViewOptions);
     setShowSortOptions(false); // Close the sort options dropdown
-    console.log(token);
   };
 
-
   const truncateText = (text, maxLength) => {
-    if (text.length > maxLength) {
-      return text.substring(0, maxLength) + "....."; // Add ellipsis if text is truncated
+    if (text && text.length > maxLength) {
+      return text.substring(0, maxLength) + ".....";
     }
     return text;
   };
 
-  const renderMediaOrTruncateText = (media, text) => {
-    const maxTextLength = 450; // Define the maximum length for text display
-
+  const renderMediaOrTruncateText = (media, content) => {
+    const maxTextLength = 450;
     if (media) {
-      // If media is present, call renderMedia function
-      return renderMedia(media, text);
+      return renderMedia(media, content);
     } else {
-      // If media is not present, call truncateText function
-      return <p>{truncateText(text, maxTextLength)}</p>;
+      return <p>{truncateText(content, maxTextLength)}</p>;
     }
   };
 
   const renderMedia = (media, text) => {
     if (Array.isArray(media)) {
-      // If media is an array, assume it's a list of images and render the ImageSlider component
       return <ImageSlider slides={media} viewType={viewType} />;
     } else if (typeof media === "string") {
-      // If media is a string, check if it's a direct link to an image and render it
       if (media.match(/\.(jpeg|jpg|gif|png)$/) != null) {
         return <img src={media} alt={text} className="post-image" />;
       } else {
-        // If it's not an image URL, assume it's a video and render using React Player
         return (
           <ReactPlayer url={media} width="540px" height="500px" controls />
         );
       }
     } else {
-      // If media is neither an array nor a string, return null or handle it according to your requirements
       return null;
     }
   };
-  
 
   const renderMediaWithCount = (media, text) => {
     if (!media) {
-      // If media is not provided, return CiViewList component
       return <CiViewList className="compact-post-card-image-compensation" />;
     } else if (Array.isArray(media)) {
-      // If media is an array
       if (media.length === 1) {
-        // If it contains only one image, render the image
         return (
           <img src={media[0]} alt={text} className="compact-post-card-image" />
         );
       } else {
-        // If it contains multiple images, render the first image and return the count
         return (
           <>
             <img
@@ -188,37 +321,34 @@ const Content = () => {
         );
       }
     } else if (typeof media === "string") {
-      // If media is a string
       if (media.match(/\.(jpeg|jpg|gif|png)$/) != null) {
-        // If it's a direct link to an image, render the image
         return (
           <img src={media} alt={text} className="compact-post-card-image" />
         );
       } else {
-        // If it's not an image URL, assume it's a video and return CiViewList component
         return <RiVideoFill className="compact-post-card-image-compensation" />;
       }
     } else {
-      // If media is neither an array nor a string, return CiViewList component
       return <CiViewList className="compact-post-card-image-compensation" />;
     }
   };
 
-  
-
   return (
     <div className="container">
+      <ToastContainer />
+
       <div className="choice-above-posts">
         <div className="content-sort-type">
           <button className="content-drop-down-list" onClick={handleSortTypes}>
-            Best <MdKeyboardArrowDown />
+            {sortingType.charAt(0).toUpperCase() + sortingType.slice(1)}{" "}
+            <MdKeyboardArrowDown />
           </button>
           {showSortOptions && (
             <div className="options-content-sort-drop-down-list">
-              <button>Hot</button>
-              <button>Rising</button>
-              <button>Top</button>
-              <button>New</button>
+              <button onClick={() => handleSortingOption("Best")}>Best</button>
+              <button onClick={() => handleSortingOption("Hot")}>Hot</button>
+              <button onClick={() => handleSortingOption("Top")}>Top</button>
+              <button onClick={() => handleSortingOption("New")}>New</button>
             </div>
           )}
         </div>
@@ -230,10 +360,20 @@ const Content = () => {
           </button>
           {showViewOptions && (
             <div className="options-content-view-drop-down-list">
-              <button onClick={() => { setViewType("card"); setShowViewOptions(false); }}>
+              <button
+                onClick={() => {
+                  setViewType("card");
+                  setShowViewOptions(false);
+                }}
+              >
                 <BsViewStacked />
               </button>
-              <button onClick={() => { setViewType("compact"); setShowViewOptions(false); }}>
+              <button
+                onClick={() => {
+                  setViewType("compact");
+                  setShowViewOptions(false);
+                }}
+              >
                 <BsViewList />
               </button>
             </div>
@@ -249,55 +389,62 @@ const Content = () => {
             <p>No posts to display.</p>
           ) : (
             posts.map((post) =>
-              !hiddenPosts[post.id] ? (
-                // Conditionally render either PostCard or CompactPostCard based on viewType
-                viewType === "card" ? (
-                  <PostCard
-                    key={post.id}
-                    post={post}
-                    joinStates={joinStates}
-                    saveStates={saveStates}
-                    handleJoinClick={handleJoinClick}
-                    handleSaveClick={handleSaveClick}
-                    handleHideClick={handleHideClick}
-                    handleUpvoteClick={handleUpvoteClick}
-                    handleDownvoteClick={handleDownvoteClick}
-                    getCommunityName={getCommunityName}
-                    getCommunityImage={getCommunityImage}
-                    renderMediaOrTruncateText={renderMediaOrTruncateText}
-                  />
+              post.postId &&
+              post.name &&
+              post.image &&
+              post.title &&
+              (post.media || post.content) &&
+              post.likes &&
+              post.comments &&
+              post.time &&
+              post.reason ? (
+                !hiddenPosts[post.postId] ? (
+                  viewType === "card" ? (
+                    <PostCard
+                      key={post.postId}
+                      post={post}
+                      joinStates={joinStates}
+                      saveStates={saveStates}
+                      handleJoinClick={handleJoinClick}
+                      handleSaveClick={handleSaveClick}
+                      handleReportClick={handleReportClick}
+                      handleHideClick={handleHideClick}
+                      handleUpvoteClick={handleUpvoteClick}
+                      handleDownvoteClick={handleDownvoteClick}
+                      renderMediaOrTruncateText={renderMediaOrTruncateText}
+                    />
+                  ) : (
+                    <CompactPostCard
+                      key={post.id}
+                      post={post}
+                      joinStates={joinStates}
+                      saveStates={saveStates}
+                      handleJoinClick={handleJoinClick}
+                      handleSaveClick={handleSaveClick}
+                      handleReportClick={handleReportClick}
+                      handleHideClick={handleHideClick}
+                      handleUpvoteClick={handleUpvoteClick}
+                      handleDownvoteClick={handleDownvoteClick}
+                      renderMediaWithCount={renderMediaWithCount}
+                      renderMedia={renderMedia}
+                    />
+                  )
                 ) : (
-                  <CompactPostCard
-                    key={post.id}
-                    post={post}
-                    joinStates={joinStates}
-                    saveStates={saveStates}
-                    handleJoinClick={handleJoinClick}
-                    handleSaveClick={handleSaveClick}
-                    handleHideClick={handleHideClick}
-                    handleUpvoteClick={handleUpvoteClick}
-                    handleDownvoteClick={handleDownvoteClick}
-                    getCommunityName={getCommunityName}
-                    getCommunityImage={getCommunityImage}
-                    renderMediaWithCount={renderMediaWithCount}
-                    renderMedia={renderMedia}
-                  />
+                  <div key={post.postId} className="hidden-post-card">
+                    <p>Post hidden</p>
+                    <button
+                      onClick={() =>
+                        setHiddenPosts((prevHiddenPosts) => ({
+                          ...prevHiddenPosts,
+                          [post.postId]: false,
+                        }))
+                      }
+                    >
+                      Undo
+                    </button>
+                  </div>
                 )
-              ) : (
-                <div key={post.id} className="hidden-post-card">
-                  <p>Post hidden</p>
-                  <button
-                    onClick={() =>
-                      setHiddenPosts((prevHiddenPosts) => ({
-                        ...prevHiddenPosts,
-                        [post.id]: false,
-                      }))
-                    }
-                  >
-                    Undo
-                  </button>
-                </div>
-              )
+              ) : null
             )
           )}
         </div>
@@ -307,5 +454,3 @@ const Content = () => {
 };
 
 export default Content;
-
-
