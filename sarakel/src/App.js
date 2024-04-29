ecd3f9b92944f023c76b0a79e1a13aad9828e887
KareemAbdelnabi <97126849+KareemAbdelnabi@users.noreply.github.com>
2024-04-29 18:39:43 +0300

    feat : reset password component navigation

--- sarakel/src/App.js
+++ sarakel/src/App.js
@@ -1,39 +1,32 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage/HomePage.js';
-import Settings from './Settings/SettingPage.js'
-import Moderation from './Moderation/Moderation.js'
+import Settings from './Settings/SettingPage.js';
+import Moderation from './Moderation/Moderation.js';
 import Notifications from './Notifications/Notifications.js';
 import MessagesPage from './messagespage/messagespage.js';
 import CreatePost from './CreatePost/CreatePost.js';
-import ProfilePage from './ProfilePage/ProfilePage.js'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import Search from './Search/Search.js'
+import ProfilePage from './ProfilePage/ProfilePage.js';
+import Search from './Search/Search.js';
+import ResetPasswordModal from './HomePage/Components/Reset/ResetPassword.js'; // Import the ResetPasswordModal component
 import { AuthProvider } from './HomePage/Components/AuthContext.js';
-import messagespage from './messagespage/messagespage.js'
-//import { Route } from 'react-router-dom';
-//import NavBar from './HomePage/Components/NavBar/NavBar.js'
+
 function App() {
   return (      
     <AuthProvider>
-        <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route index  element={<HomePage />} />
-          <Route path='settings' element={<Settings />} />
-          <Route path = 'MessagesPage' element={<MessagesPage />}></Route>
-          <Route path ='Moderation' element={<Moderation />}></Route>
-          <Route path = 'CreatePost' element={<CreatePost />}></Route>
-          <Route path = 'ProfilePage' element={<ProfilePage />}></Route>
-          <Route path='Search' element={<Search />}></Route>
-          <Route path='Notifications' element={<Notifications />}></Route>
-          <Route path='MessagesPage' element={<MessagesPage></MessagesPage>}></Route>
+          <Route index element={<HomePage />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="messagespage" element={<MessagesPage />} />
+          <Route path="moderation" element={<Moderation />} />
+          <Route path="createpost" element={<CreatePost />} />
+          <Route path="profilepage" element={<ProfilePage />} />
+          <Route path="search" element={<Search />} />
+          <Route path="notifications" element={<Notifications />} />
+          <Route path="login/reset_password/:token" element={<ResetPasswordModal />} /> 
         </Routes>
       </BrowserRouter>
     </AuthProvider>
-
-
-    
   );
 }
 
