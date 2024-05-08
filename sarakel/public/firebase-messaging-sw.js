310440aabe440f12219c13b1e63721a518360f71
tonyatef <104167385+tonyatef@users.noreply.github.com>
2024-05-08 21:59:51 +0300

    Add files via upload

---
+++ sarakel/public/firebase-messaging-sw.js
@@ -0,0 +1,11 @@
+importScripts("https://www.gstatic.com/firebasejs/7.20.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/7.20.0/firebase-messaging.js");
+
+firebase.initializeApp({   apiKey: "AIzaSyDC4_03h716SWY5iSncgEJCM18_R1bb6kA",
+authDomain: "final-sarakel.firebaseapp.com",
+projectId: "final-sarakel",
+storageBucket: "final-sarakel.appspot.com",
+messagingSenderId: "74358882817",
+appId: "1:74358882817:web:af81adb71413daa871b600" });
+
+const messaging = firebase.messaging();
